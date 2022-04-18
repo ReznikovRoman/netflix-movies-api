0f1a2d6deedcac4978364c67be610e50166a2244
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-04-18 16:40:24 +0400

    #16: feat: add new `FilmDetail` fields

--- src/schemas/films.py
+++ src/schemas/films.py
@@ -1,6 +1,22 @@
+import datetime
+from enum import Enum
+
 from .base import BaseIdOrjsonSchema
 from .genres import GenreDetail
 from .persons import PersonList
+
+
+class FilmAgeRating(str, Enum):
+    """Возрастной рейтинг фильма.
+
+    Список возрастных рейтингов: https://bit.ly/3xuSpB0.
+    """
+
+    GENERAL = "G"
+    PARENTAL_GUIDANCE = "PG"
+    PARENTS = "PG-13"
+    RESTRICTED = "R"
+    ADULTS = "NC-17"
 
 
 class FilmDetail(BaseIdOrjsonSchema):
@@ -9,6 +25,8 @@ class FilmDetail(BaseIdOrjsonSchema):
     title: str
     imdb_rating: float | None
     description: str
+    release_date: datetime.date
+    age_rating: FilmAgeRating
     genre: list[GenreDetail]
     actors: list[PersonList]
     writers: list[PersonList]
