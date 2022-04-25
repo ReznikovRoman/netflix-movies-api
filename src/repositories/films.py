3539c87a6e27551f3fbf18605eab018ec2b425e5
Pycb <fessmn@ya.ru>
2022-04-25 02:55:35 -0700

    #9: feat: added cache for multiple objects for Films

--- src/repositories/films.py
+++ src/repositories/films.py
@@ -1,3 +1,4 @@
+import json
 from functools import lru_cache
 from typing import ClassVar
 from uuid import UUID
@@ -12,13 +13,13 @@ from db.elastic import get_elastic
 from db.redis import get_redis
 from schemas.films import FilmDetail, FilmList
 
-from .base import ElasticRepositoryMixin, ElasticSearchRepositoryMixin
+from .base import ElasticRepositoryMixin, ElasticSearchRepositoryMixin, RedisRepositoryMixin
 
 
 FILM_CACHE_EXPIRE_IN_SECONDS = 60 * 5
 
 
-class FilmRepository(ElasticSearchRepositoryMixin, ElasticRepositoryMixin):
+class FilmRepository(ElasticSearchRepositoryMixin, ElasticRepositoryMixin, RedisRepositoryMixin):
     """Репозиторий для работы с данными Фильмов."""
 
     es_index_name: ClassVar[str] = "movies"
@@ -42,10 +43,10 @@ class FilmRepository(ElasticSearchRepositoryMixin, ElasticRepositoryMixin):
         return FilmDetail(**film_doc)
 
     async def get_film_from_redis(self, film_id: UUID) -> FilmDetail:
-        data = await self.redis.get(str(film_id))
-        if not data:
+        film = await self.redis.get(str(film_id))
+        if not film:
             return None
-        return FilmDetail.parse_raw(data)
+        return FilmDetail.parse_raw(film)
 
     async def put_film_to_redis(self, film_id: UUID, film_json_data):
         await self.redis.set(str(film_id), film_json_data, ex=FILM_CACHE_EXPIRE_IN_SECONDS)
@@ -62,6 +63,16 @@ class FilmRepository(ElasticSearchRepositoryMixin, ElasticRepositoryMixin):
         films_docs = await self.get_documents_from_elastic(request_body=request_body, sort=sort)
         return parse_obj_as(list[FilmList], films_docs)
 
+    async def get_all_films_from_redis(self, string_for_hash):
+        films = await self.redis.get(self.get_hash(string_for_hash))
+        if not films:
+            return None
+        return [FilmList.parse_raw(film) for film in json.loads(films)]
+
+    async def put_all_films_to_redis(self, films, string_for_hash):
+        json_str = json.dumps([film.json() for film in films])
+        await self.redis.set(self.get_hash(string_for_hash), json_str, ex=FILM_CACHE_EXPIRE_IN_SECONDS)
+
     async def search_films_from_elastic(
         self, page_size: int, page_number: int, query: str, sort: str | None = None,
     ) -> list[FilmList]:
@@ -73,6 +84,16 @@ class FilmRepository(ElasticSearchRepositoryMixin, ElasticRepositoryMixin):
         )
         films_docs = await self.get_documents_from_elastic(request_body=request_body, sort=sort)
         return parse_obj_as(list[FilmList], films_docs)
+
+    async def get_search_films_from_redis(self, string_for_hash):
+        films = await self.redis.get(self.get_hash(string_for_hash))
+        if not films:
+            return None
+        return [FilmList.parse_raw(film) for film in json.loads(films)]
+
+    async def put_search_films_to_redis(self, films, string_for_hash):
+        json_str = json.dumps([film.json() for film in films])
+        await self.redis.set(self.get_hash(string_for_hash), json_str, ex=FILM_CACHE_EXPIRE_IN_SECONDS)
 
 
 @lru_cache()
--- src/services/films.py
+++ src/services/films.py
@@ -22,25 +22,47 @@ class FilmService:
             await self.film_repo.put_film_to_redis(film_id, film.json())
         return film
 
+    # async def get_all_films(
+    #     self, page_size: int, page_number: int, genre: str | None = None, sort: str | None = None,
+    # ) -> list[FilmList]:
+    #     # TODO add redis cache check
+    #     films = await self.film_repo.get_all_films_from_elastic(
+    #         page_size=page_size,
+    #         page_number=page_number,
+    #         genre=genre,
+    #         sort=sort,
+    #     )
+    #     return films
+
     async def get_all_films(
         self, page_size: int, page_number: int, genre: str | None = None, sort: str | None = None,
     ) -> list[FilmList]:
-        # TODO add redis cache check
-        films = await self.film_repo.get_all_films_from_elastic(
-            page_size=page_size,
-            page_number=page_number,
-            genre=genre,
-            sort=sort,
-        )
+        string_for_hash = f"all_films{page_size}{page_number}{genre}{sort}"
+        films = await self.film_repo.get_all_films_from_redis(string_for_hash)
+        if not films:
+            films = await self.film_repo.get_all_films_from_elastic(
+                page_size=page_size,
+                page_number=page_number,
+                genre=genre,
+                sort=sort,
+            )
+            if not films:
+                return None
+            await self.film_repo.put_all_films_to_redis(films=films, string_for_hash=string_for_hash)
         return films
 
     async def search_films(
         self, page_size: int, page_number: int, query: str, sort: str | None = None,
     ) -> list[FilmList]:
-        # TODO add redis cache check
-        films = await self.film_repo.search_films_from_elastic(
-            page_size=page_size, page_number=page_number, query=query, sort=sort,
-        )
+        string_for_hash = f"search_films{page_size}{page_number}{sort}"
+        films = await self.film_repo.get_search_films_from_redis(string_for_hash)
+        if not films:
+            films = await self.film_repo.search_films_from_elastic(
+                page_size=page_size, page_number=page_number, query=query, sort=sort,
+            )
+            if not films:
+                return None
+            await self.film_repo.put_search_films_to_redis(films=films, string_for_hash=string_for_hash)
         return films
 
 
