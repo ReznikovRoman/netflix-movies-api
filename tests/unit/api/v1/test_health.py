799ab9301366ea94c7d206fb5a05e2f3f4be88cb
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-04-29 14:23:06 +0400

    #23: test: move unit tests to the `unit` directory

--- tests/unit/api/v1/test_health.py
+++ tests/unit/api/v1/test_health.py
@@ -1,13 +1,8 @@
 from fastapi.testclient import TestClient
 
-from core.config import get_settings
-
-
-settings = get_settings()
-
 
 def test_ok(client: TestClient):
     """Ручка /healthcheck отдает 200 статус."""
-    response = client.get(f"{settings.API_V1_STR}/healthcheck")
+    response = client.get("/api/v1/healthcheck")
 
     assert response.status_code == 200
