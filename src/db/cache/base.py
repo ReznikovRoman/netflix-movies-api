bb57c4895adad66a74e8d87b3bb91bf0ce2ab809
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-05-07 18:25:46 +0400

    refactor: use abstract classes instead of protocols

--- src/db/cache/base.py
+++ src/db/cache/base.py
@@ -1,18 +1,21 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Protocol
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
 
 
 if TYPE_CHECKING:
     from common.types import seconds
 
 
-class AsyncCache(Protocol):
+class AsyncCache(ABC):
     """Асинхронный кеш."""
 
+    @abstractmethod
     async def get(self, key: str) -> Any:
         """Получение данных из кеша по ключу `key`."""
 
+    @abstractmethod
     async def set(self, key: str, data: Any, *, timeout: seconds | None = None) -> bool:
         """Сохранение данных с заданным ttl и ключом.
 
@@ -25,5 +28,6 @@ class AsyncCache(Protocol):
             bool: были ли данные сохранены успешно.
         """
 
+    @abstractmethod
     def get_timeout(self, timeout: seconds | None = None) -> int | None:
         """Получение `ttl` (таймаута) для записи в кеше."""
--- src/db/storage/base.py
+++ src/db/storage/base.py
@@ -1,20 +1,24 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Protocol
+from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Any
 
 
 if TYPE_CHECKING:
     from common.types import Id, Query
 
 
-class AsyncStorage(Protocol):
+class AsyncStorage(ABC):
     """Асинхронная база данных."""
 
+    @abstractmethod
     async def get_by_id(self, instance_id: Id, *args, **kwargs) -> Any:
         """Получение записи из БД по ID `instance_id`."""
 
+    @abstractmethod
     async def search(self, query: Query, *args, **kwargs) -> Any:
         """Получение записей из БД с запросом `query`."""
 
+    @abstractmethod
     async def get_all(self, *args, **kwargs) -> Any:
         """Получение всех записей."""
