6f85e14b0646f36ee9024f5118defafff1c1a5b7
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-05-07 22:32:03 +0400

    refactor: use Enum to store service messages

--- src/api/v1/endpoints/genres.py
+++ src/api/v1/endpoints/genres.py
@@ -2,6 +2,7 @@ from uuid import UUID
 
 from fastapi import APIRouter, Depends, HTTPException, status
 
+from api.constants import MESSAGE
 from common.exceptions import NotFoundError
 from schemas.genres import GenreDetail
 from services.genres import GenreService, get_genre_service
@@ -16,7 +17,7 @@ async def get_genre(uuid: UUID, genre_service: GenreService = Depends(get_genre_
     try:
         genre = await genre_service.get_genre_by_id(uuid)
     except NotFoundError:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="genre not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=MESSAGE.GENRE_NOT_FOUND.value)
 
     return genre
 
