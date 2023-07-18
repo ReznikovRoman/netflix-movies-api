6f33c7b9237fced8af4894dfb314253789759d5b
Roman Reznikov <44291988+ReznikovRoman@users.noreply.github.com>
2023-07-18 19:53:12 +0300

    Update health.py

--- src/movies/api/v1/handlers/health.py
+++ src/movies/api/v1/handlers/health.py
@@ -6,4 +6,4 @@ router = APIRouter(tags=["health"])
 @router.get("/", summary="Service health")
 async def healthcheck():
     """Check service health."""
-    return {"status": "ok"}
+    return {"status": "ok 2"}
